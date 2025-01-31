eff9451c723608afd10fdbfa4167e0dd9781b406
JP Meijers <jp.meijers@canonical.com>
2025-01-31 17:25:23 +0200

    Add headings for test steps
    Change to medium arm runner (large is default)

--- tests/run-test-suite.sh
+++ tests/run-test-suite.sh
@@ -20,7 +20,7 @@ LINUXPTP_TESTSUITE_VERSION="8652e8b"
 
 CLKNETSIM_VERSION="64acc4fd0ee92c2bafd9d1cf8097eb3632da685b"
 
-
+echo "# Setting snap aliases"
 sudo snap alias linuxptp.ptp4l ptp4l
 sudo snap alias linuxptp.hwstamp-ctl hwstamp_ctl
 sudo snap alias linuxptp.nsm nsm
@@ -31,18 +31,22 @@ sudo snap alias linuxptp.timemaster timemaster
 sudo snap alias linuxptp.ts2phc ts2phc
 sudo snap alias linuxptp.tz2alt tz2alt
 
+echo -e "\n# Cloning linuxptp-testsuite"
 # Remove test suite if it already exists, and clone again
 rm -rf linuxptp-testsuite
 git clone https://github.com/mlichvar/linuxptp-testsuite.git
 cd linuxptp-testsuite
 git checkout $LINUXPTP_TESTSUITE_VERSION
 
+echo -e "\n# Cloning clknetsim"
 git clone https://github.com/mlichvar/clknetsim
 cd clknetsim
 git checkout $CLKNETSIM_VERSION
+echo -e "\n# Building clknetsim"
 make
 # Replace clknetsim's LD_PRELOAD with a copy of the shared object contained inside the snap, which uses the correct glibc version for the base
 sed -i -e 's/LD_PRELOAD=${CLKNETSIM_PRELOAD:+$CLKNETSIM_PRELOAD:}$CLKNETSIM_PATH/CLKNETSIM_SO_PATH=\/snap\/linuxptp\/current/g' clknetsim.bash
 
+echo -e "\n# Running tests"
 cd ..
 ./run
